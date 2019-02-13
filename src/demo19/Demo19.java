c6261c3aecc6f040acd31e0f1a5116f4db7200be
poriaux <michel.poriaux@condorcet.be>
2019-02-13 12:24:31 +0100

    commit3

--- src/demo19/Demo19.java
+++ src/demo19/Demo19.java
@@ -15,7 +15,7 @@ public class Demo19 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ok");
+        System.out.println("ok2");
     }
     
 }
